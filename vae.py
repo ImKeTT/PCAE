c05f708cb17a1e51f852638ed25d0b4641576427
Haoqin Tu <thq415_ic@yeah.net>
2022-09-28 15:15:28 +0800

    change pooling to mean pooling in latent space

--- vae.py
+++ vae.py
@@ -20,9 +20,7 @@ class BartPooler(nn.Module):
         self.activation = nn.Tanh()
 
     def forward(self, hidden_states):
-        # We "pool" the model by simply taking the hidden state corresponding
-        # to the first token.
-        first_token_tensor = hidden_states[:, 0]
+        first_token_tensor = hidden_states.mean(-1)
         pooled_output = self.dense(first_token_tensor)
         pooled_output = self.activation(pooled_output)
         return pooled_output
